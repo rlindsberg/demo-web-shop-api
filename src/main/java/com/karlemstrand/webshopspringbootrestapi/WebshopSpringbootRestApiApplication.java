f5c0e52ee70225e0d976a0abfe84d820f87a1f4e
Roderick Karlemstrand <roderick@karlemstrand.com>
2024-03-24 22:24:05 +0100

    create project with springboot init

---
+++ src/main/java/com/karlemstrand/webshopspringbootrestapi/WebshopSpringbootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.karlemstrand.webshopspringbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopSpringbootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopSpringbootRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karlemstrand/webshopspringbootrestapi/WebshopSpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karlemstrand.webshopspringbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopSpringbootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
