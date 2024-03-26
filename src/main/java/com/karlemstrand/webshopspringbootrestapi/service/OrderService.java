758c7e6270918064813c4cf445bbf85fdca1a696
Roderick Karlemstrand <roderick@karlemstrand.com>
2024-03-26 01:51:00 +0100

    Impl get all user orders

--- src/main/java/com/karlemstrand/webshopspringbootrestapi/service/OrderService.java
+++ src/main/java/com/karlemstrand/webshopspringbootrestapi/service/OrderService.java
@@ -4,8 +4,11 @@ import com.karlemstrand.webshopspringbootrestapi.dtos.OrderDto;
 import com.karlemstrand.webshopspringbootrestapi.entity.Order;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface OrderService {
     List<Order> getAllOrders();
+    List<Order> getAllOrdersByUserId(UUID userId);
+
     OrderDto createOrder(OrderDto orderDto);
 }
